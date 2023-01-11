1f1f3e0661d5e4ff220f68d7ccbe5c12e9266822
Roman_Maksymuk <romamaksymuk@gmail.com>
2023-01-11 11:51:51 +0200

    implemented sort func

--- src/middleware/pagination.ts
+++ src/middleware/pagination.ts
@@ -21,6 +21,31 @@ export interface RequestWithResult extends Request {
   paginatedResult?: Result;
 }
 
+const sortPhones = (sortType: string, phones: Phone[]) => {
+  if (sortType !== 'default') {
+    phones.sort((phoneOne, phoneTwo) => {
+      switch (true) {
+      case sortType.includes('Age'):
+        return +phoneOne.year - +phoneTwo.year;
+
+      case sortType.includes('Price'):
+        return +phoneOne.price - +phoneTwo.price;
+
+      case sortType.includes('Name'):
+        return phoneOne.name.localeCompare(phoneTwo.name);
+      default:
+        return 0;
+      }
+    });
+  }
+
+  if (sortType.includes('desc')) {
+    return phones.reverse();
+  }
+
+  return phones;
+};
+
 export const paginate = (model: Phone[] | null) => {
   return (req: RequestWithResult, res: Response, next: NextFunction) => {
     try {
@@ -28,10 +53,15 @@ export const paginate = (model: Phone[] | null) => {
         return res.status(404).send('Error!');
       }
 
-      let { page = 1, limit = model.length } = req.query;
+      const {
+        pageParam = 1,
+        limitParam = model.length,
+        sortByParam = 'default',
+      } = req.query;
 
-      page = Number(page);
-      limit = Number(limit);
+      const page = Number(pageParam);
+      const limit = Number(limitParam);
+      const sortBy = sortByParam + '';
 
       const startIndex = (page - 1) * limit;
       const endIndex = page * limit;
@@ -53,7 +83,7 @@ export const paginate = (model: Phone[] | null) => {
         };
       }
 
-      result.results = model.slice(startIndex, endIndex);
+      result.results = sortPhones(sortBy, model.slice(startIndex, endIndex));
 
       result.totalPages = Math.ceil(count / limit);
       result.currentPage = page;
