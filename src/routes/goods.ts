1fe387902cb050bdf1281c45660eb7a53291df35
Roman_Maksymuk <romamaksymuk@gmail.com>
2023-01-12 17:50:47 +0200

    created getOne

--- src/routes/goods.ts
+++ src/routes/goods.ts
@@ -11,6 +11,6 @@ const phones: Phone[] | null = goodsService.getAll();
 router.get('/products', paginate(phones), goodController.getAll);
 router.get('/products/getNew', goodController.getNewGoods);
 router.get('/products/getDiscounted', goodController.getDiscountedGoods);
-// router.get('/:goodId', goodController.getOne);
+router.get('/products/:goodId', goodController.getOne);
 // router.post('/', goodController.add);
 // router.delete('/:goodId', goodController.remove);
